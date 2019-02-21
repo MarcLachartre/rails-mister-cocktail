9c469c76740d81c4c706ae64b0aa27736015f01a
Marc Lachartre <marc.lachartre@gmail.com>
2019-02-21 18:39:58 +0100

    half of the exercise done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "White Russian")
